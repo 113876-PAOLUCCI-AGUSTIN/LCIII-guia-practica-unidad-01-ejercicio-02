5b6d76acac8af6babad112d7957cbf0187abd04d
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-20 21:28:27 -0300

    Metodo creado y salida por pantalla.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 /*
-* LCIII-guia-practica-unidad-01-ejercicio-02
+* LC_III-guía-práctica-unidad-01-ejercicio-02
 * Hacer un programa que ingrese el precio de un artículo a la venta y calcule
 * el precio con IVA.
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el precio del artículo...");
+        Double precioArticulo = teclado.nextDouble();
+        System.out.println("Precio del articulo IVA incl.:"+calcularIVA(precioArticulo));
+    }
+    private static Double calcularIVA(Double precio) {
+        return precio * 1.21;
     }
 }
