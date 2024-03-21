221c28f94552332adf7202e261979d2a80b87cb8
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-20 21:15:06 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*
+* LCIII-guia-practica-unidad-01-ejercicio-02
+* Hacer un programa que ingrese el precio de un artículo a la venta y calcule
+* el precio con IVA.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
